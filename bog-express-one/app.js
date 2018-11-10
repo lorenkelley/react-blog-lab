39a2d93348a0b1d8a9465d1692c5fb4ad10b5613
CB_ATL_115_Loren_Kelly <cb_atl_115_loren_kelly@MacBook-Air-90.local>
2018-11-10 15:23:26 -0500

    trying to get server to run on port 3001

--- bog-express-one/app.js
+++ bog-express-one/app.js
@@ -9,11 +9,20 @@ app.use(logger('dev'))
 app.use(express.urlencoded({
     extended: true
 }))
+
+app.use(express.static(`${__dirname}/client/build`))
+
+
 app.use(express.json())
 
 
 
 app.use('/', routes)
+
+  //below your api routes
+  app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+  })
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
--- bog-express-one/controllers/creatureController.js
+++ bog-express-one/controllers/creatureController.js
@@ -31,7 +31,6 @@ delete: (req, res) => {
         res.send()
     })
 }
-
 }
 
 
